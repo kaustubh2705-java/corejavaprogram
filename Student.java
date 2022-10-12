198b0fe283895ed18ad1df9befc76a6a606a2d2f
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-10-12 17:51:03 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.ptpl;
+
+public class Student {
+    int rollNumber; // insatance variable
+	String Name;
+	double Fees;
+	boolean Result;
+	char sex;
+	static int count; // non instance variable
+
+}
---
+++ TestStudent.java
@@ -0,0 +1,17 @@
+package com.ptpl;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+
+		Student s1=new Student ();
+		
+		System.out.println(s1.rollNumber);
+		System.out.println(s1.Name);
+		System.out.println(s1.Fees);
+		System.out.println(s1.Result);
+		System.out.println((int)s1.sex);
+		
+	}
+
+}
