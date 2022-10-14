258438c167c58559a539a7e828ffca7d48d468ff
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-10-14 11:43:48 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,21 @@
+package com.ptpl;
+
+public class Data {
+
+	private int x,y,z;
+	
+	void setData(int px,int py,int pz) {
+		
+		x=px;
+		y=py;
+		z=pz;
+		
+		
+	}
+	
+	void showData() {
+		System.out.println(x+" "+y+" "+z);
+	}
+
+	
+}
---
+++ TestStudent1.java
@@ -0,0 +1,20 @@
+package com.ptpl;
+
+public class TestStudent1 {
+
+	public static void main(String[] args) {
+
+		Data d1=new Data();
+		
+		
+		d1.setData(100,200,300);
+		
+		d1.showData();
+		
+		
+		
+			
+		
+	}
+
+}
