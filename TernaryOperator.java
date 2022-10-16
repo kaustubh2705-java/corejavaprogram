3d9a8edd9a49c3f06b1b119ceb303221a16e5b95
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-10-16 15:50:31 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,16 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+
+		// Conditional ternary (short-hand if- else)
+		//. ?:
+		
+		int y = (9>10) ? 100 : 200;
+		
+		
+		System.out.println(y);
+		
+	}
+
+}
