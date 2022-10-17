d43014ac3111c66721aa3621a8e4ca9fe5f16fd0
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-10-17 15:42:08 +0530

    Add files via upload

---
+++ Teastmycode.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Teastmycode {
+
+	public static void main(String[] args) {
+
+		int n1,n2,substract;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any two number");
+		
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		
+		 substract=n1-n2;
+		
+		
+		System.out.println(substract);
+	}
+
+}
---
+++ Testmycode2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Testmycode2 {
+
+	public static void main(String[] args) {
+
+		int n1,n2,multiply;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any two number");
+		
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		
+		multiply=n1*n2;
+	
+		System.out.println(multiply);
+		
+		
+	
+	}
+
+}
