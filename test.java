86d4490400a3e71eb4ddfbfe3663726dc64d81a8
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-10-17 15:39:15 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		int i,n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the range");
+		n=sc.nextInt();
+		for(i=n;i>=1;i--)
+		
+		System.out.print(i+" ");
+	}
+
+}
