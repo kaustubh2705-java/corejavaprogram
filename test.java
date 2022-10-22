c919c1ce47aec5d686f960242e90b2cd5c5f77a4
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-10-22 23:22:24 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,16 +1,30 @@
+package practiceex;
+
 import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
-
-		int i,n;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the range");
-		n=sc.nextInt();
-		for(i=n;i>=1;i--)
-		
-		System.out.print(i+" ");
+      char ch;
+      System.out.println("Enter any character");
+      Scanner sc=new Scanner(System.in);
+      ch=sc.next().charAt(0);
+      if(ch>=65 && ch<=90) {
+    	  System.out.println("capital letter");
+      }
+      else if(ch>=97 && ch<=122) {
+    	  System.out.println("lower case");
+      }
+      else if(ch>=48 && ch<=57) {
+    	  System.out.println("I is a digit");
+      }
+      else if((ch>=0 && ch<=47)|| (ch>=58 && ch<=64)||(ch>=91 && ch<=96)||(ch>=123 && ch<=127)) {
+    	  System.out.println("Special symbol");
+      }
+      else {
+  		System.out.println("other");
+	}
+	
 	}
 
 }
