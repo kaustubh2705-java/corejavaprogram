e25db4f23ba291086c2df0a91442a645914e3b3e
kaustubh2705-java <113851284+kaustubh2705-java@users.noreply.github.com>
2022-11-12 11:59:13 +0530

    Add files via upload

---
+++ AbsoluteValue.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+	public static void main(String[] args) {
+		
+		int x;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Absolute Value");
+		
+		x=sc.nextInt();
+		
+		if(x<0) {
+			System.out.println(x*(-1));
+		}
+		else {
+			
+			System.out.println(x);
+		}
+		
+		
+				
+				
+				
+	}
+
+}
+
